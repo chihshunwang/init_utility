55225df1d445f16d2bc2cbc29747771f7adbe80d
Sam Wang <chihshun.wang@japancv.co.jp>
2020-11-09 13:59:04 +0900

    Add the init script which can hel to upgrade os and install docker.

---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+install_docker() {
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
+
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+}
+
+install_docker_compose() {
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
+
+# update
+sudo apt-get update
+
+# upgrade
+sudo apt-get -y upgrade
+
+# install docker
+install_docker
+
+# install docker compose
+install_docker_compose
